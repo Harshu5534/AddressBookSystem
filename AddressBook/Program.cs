f46f32d87eb0fb95b450ca9ad23f8bcc6ae93ec0
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-19 16:17:20 +0530

    [Harshal]Add:Address Book Details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
 Console.WriteLine("------WelCome To Address Book Program------");
+AddAddressBook book = new AddAddressBook();
+book.AddContacts();
+book.Display();
