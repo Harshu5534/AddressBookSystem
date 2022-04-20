4d21afb716a554cd9d27534ab16b23499eb95339
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-20 20:14:47 +0530

    [Harshal]Add:Edit Contact From AddressBook

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
-﻿using AddressBook;
+﻿using System;
+using AddressBook;
 
 namespace LogicalProblems
 {
@@ -8,7 +9,7 @@ namespace LogicalProblems
         {
             Console.WriteLine("------WelCome To Address Book Program------");
             bool end = true;
-            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.End Of Program");
+            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.EditContact\n4.End Of Program");
             Contact contact = new Contact();
             AddressBookMain addContact = new AddressBookMain();
             while (end)
@@ -24,6 +25,9 @@ namespace LogicalProblems
                         addContact.Display();
                         break;
                     case 3:
+                        addContact.EditContact();
+                        break;
+                    case 4:
                         end = false;
                         Console.WriteLine("Program Is Ended");
                         break;
